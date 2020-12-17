68424dd362b83867671f4c271d7d1cb41bb1121c
Sergey Amozov <sergey_amozov@belkatechnologies.com>
2020-12-17 07:29:44 +0200

    Добавил камни, костры, и немного света

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,21 @@ public class Player : MonoBehaviour
 {
     public float health = 100f;
     [SerializeField] private GUISkin mySkin;
+    [SerializeField] private GameObject lamp;
     
     private void OnGUI()
     {
         GUI.backgroundColor = Color.red;
         //GUI.skin = mySkin;
         GUI.HorizontalScrollbar(new Rect (20,20,200,20), 0, health,0, 100);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            lamp.SetActive(!lamp.activeSelf);
+        }
+
     }
 }
