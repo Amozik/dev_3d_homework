25f1270052e26450d7f03f2c9794b9c123720151
Sergey Amozov <sergey_amozov@belkatechnologies.com>
2020-12-23 22:37:43 +0200

    IK контролы и Radoll

---
+++ Assets/Scripts/IKControl.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class IKControl : MonoBehaviour
+{
+    [SerializeField] private Transform _rightHandObj = null;
+    [SerializeField] private Transform _leftHandObj = null;
+    [SerializeField] private Transform _lookObj = null;
+    [SerializeField] private LayerMask _rayLayer;
+
+    [SerializeField] private bool _ikActive = false;
+    
+    private Animator _animator;
+    private Transform _headPos;
+    private RaycastHit _hit;
+    
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _headPos = _animator.GetBoneTransform(HumanBodyBones.Head);
+    }
+
+    private void OnAnimatorIK()
+    {
+        if (_animator)
+        {
+            if (_ikActive)
+            {
+                if (_lookObj != null)
+                {
+                    Vector3 direction = _lookObj.position - _headPos.position;// + Vector3.up;
+
+                    if (Physics.Raycast(_headPos.position, direction, out _hit, 2.0f, _rayLayer))
+                    {
+                        Debug.DrawRay(_headPos.position, direction, Color.yellow);
+                        _animator.SetLookAtWeight(1);
+                        _animator.SetLookAtPosition(_lookObj.position);
+                    }
+                    else
+                    {
+                        Debug.DrawRay(_headPos.position, direction, Color.red);
+                    }
+
+                }
+                
+                if (_rightHandObj != null)
+                {
+                    _animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+                    _animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+                    _animator.SetIKPosition(AvatarIKGoal.RightHand, _rightHandObj.position);
+                    _animator.SetIKRotation(AvatarIKGoal.RightHand, _rightHandObj.rotation);
+                }
+                
+                if (_leftHandObj != null)
+                {
+                    _animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+                    _animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+                    _animator.SetIKPosition(AvatarIKGoal.LeftHand, _leftHandObj.position);
+                    _animator.SetIKRotation(AvatarIKGoal.LeftHand, _leftHandObj.rotation);
+                }
+
+            }
+            else
+            {
+                _animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+                _animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
+                _animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 0);
+                _animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 0);
+                _animator.SetLookAtWeight(0);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RagdollControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RagdollControl : MonoBehaviour
+{
+    [SerializeField] private bool _isActive = false;
+
+    private bool _prevIsActive = false;
+    private Rigidbody[] _rigidbodies;
+    private Collider[] _colliders;
+    private Animator _animator;
+    private NavMeshAgent _navMesh;
+    
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbodies = GetComponentsInChildren<Rigidbody>();
+        _colliders = GetComponentsInChildren<Collider>();
+        _navMesh = GetComponent<NavMeshAgent>();
+        
+        SetActive(_isActive);
+    }
+
+    void Update()
+    {
+        if (_isActive != _prevIsActive)
+        {
+            _prevIsActive = _isActive;
+            SetActive(_isActive);
+        }
+
+        
+        if (_navMesh.enabled && _navMesh.remainingDistance > 0 && _navMesh.remainingDistance <= _navMesh.stoppingDistance)
+        {
+            //Debug.Log(_navMesh.remainingDistance);
+            _isActive = true;
+        }
+    }
+
+    void SetActive(bool value)
+    {
+        Debug.Log("set active: "+ value);
+        _animator.enabled = !value;
+        _rigidbodies[0].isKinematic = value;
+        _colliders[0].enabled = !value;
+        _navMesh.enabled = !value;
+        
+        
+        for (int i = 1; i < _rigidbodies.Length; i++)
+        {
+            _rigidbodies[i].isKinematic = !value;
+        }
+
+        for (int i = 1; i < _colliders.Length; i++)
+        {
+            _colliders[i].enabled = value;
+        }
+        
+    }
+    
+}
